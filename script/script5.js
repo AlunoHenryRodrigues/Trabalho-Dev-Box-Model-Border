1abd48e8960bd9085e3796767b7fcc03e2e40105
AlunoHenryRodrigues <henry.20260114@institutojef.org.br>
2026-09-08 08:57:15 -0300

    feat: finalizando slide 5

---
+++ script/script5.js
@@ -0,0 +1,36 @@
+//entrada
+const direita = document.getElementById("direita");
+const esquerda = document.getElementById("esquerda");
+ 
+ 
+//processamentos e funções
+ 
+//botão direito
+document.addEventListener("keydown", function(event){
+    if (event.key === "ArrowRight"){
+        window.location.href = "slide6.html"
+    }
+})
+ 
+direita.addEventListener("click", function(){
+    window.location.href = "slide6.html"
+});
+
+
+
+//botao esquerdo
+document.addEventListener("keydown", function(event){
+
+    if (event.key === "ArrowLeft"){
+
+        window.location.href = "slide4.html"
+
+    }
+
+})
+
+esquerda.addEventListener("click", function(){
+
+    window.location.href = "slide4.html"
+
+});
